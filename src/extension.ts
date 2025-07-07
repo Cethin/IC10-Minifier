de0f8c46294716202ed49c75b86f20338b2361e9
Luke Smith <cethin@live.com>
2025-07-06 23:09:36 -0400

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-// import * as lua from 'lua-in-js';
-// import { Crc32 } from './crc32';
 import crc32 from 'crc32-ts';
-// import("crc32-ts");c
-
-// const luaEnv = lua.createEnv();
 
 const DEFINE_STRING = "define ";
 const ALIAS_STRING = "alias ";
@@ -16,22 +11,9 @@ const LINE_LENGTH = 52;
 const FILE_PREFIX = "compiled_";
 const HASH_STRING = "HASH(";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ic10compiler" is now active!');
 
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// const disposable = vscode.commands.registerCommand('ic10compiler.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from IC10Compiler!');
-	// });
 
 	const disposable = vscode.commands.registerCommand('ic10compiler.compile', async() => {
         // Get the active text editor
